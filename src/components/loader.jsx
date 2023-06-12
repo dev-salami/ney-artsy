085b4bf10803d652ddb968683ed111d2a8a8a156
khalil <salamikhalil02@gmail.com>
2023-06-12 14:16:11 +0100

    changed loading animation

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,9 +1,10 @@
-import { Audio } from "react-loader-spinner";
+// import { Audio } from "react-loader-spinner";
+import { MoonLoader } from "react-spinners";
 
 function loader() {
-  return (
-    <div className="flex justify-center items-center w-screen h-screen absolute  bg-gray-400">
-      <Audio
+	return (
+		<div className="flex justify-center items-center w-screen h-screen absolute  bg-gray-400">
+			{/* <Audio
         height="200"
         width="200"
         radius="9"
@@ -11,8 +12,13 @@ function loader() {
         ariaLabel="three-dots-loading"
         // wrapperStyle
         // wrapperClass
-      />
-    </div>
-  );
+      /> */}
+			<MoonLoader
+				color="#000000"
+				size={150}
+				speedMultiplier={0.3}
+			/>
+		</div>
+	);
 }
 export default loader;
